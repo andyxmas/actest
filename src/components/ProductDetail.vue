3078e33db2c66e5fa002c146b85bbc453f5a5a9b
Andy Christmas <andy@pixelcabin.io>
2022-03-22 15:05:20 +0000

    show a basic component with props passed from html

---
+++ src/components/ProductDetail.vue
@@ -0,0 +1,42 @@
+<template>
+  <small>Product Detail</small>
+  <div class="main-product-image">
+    <img :src="images[0]" />
+  </div>
+
+  <div class="main-product-content">
+    <h1 class="heading-2 main-product-title">
+      {{ currentVariant.title }}
+    </h1>
+
+    <div class="main-product-description rich-text">
+      {{ currentVariant.description }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, ref } from "vue";
+
+const props = defineProps({
+  images: {
+    type: Array,
+    default: () => [],
+  },
+  variants: {
+    type: Array,
+    default: () => [],
+  },
+  initialVariantId: {
+    type: Number,
+    required: true,
+  },
+});
+
+const selectedVariantId = ref(props.initialVariantId);
+const currentVariant = computed(() => {
+  return props.variants.find((variant) => {
+    return variant.id == selectedVariantId.value;
+  });
+});
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,37 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import store from "./store";
+import { createApp } from "vue/dist/vue.esm-bundler";
 
-createApp(App).use(store).mount("#app");
+/**
+ * create vue instance function
+ */
+const createVueApp = () => {
+  const app = createApp({});
+
+  /**
+   * vue components
+   * auto-import all vue components
+   */
+  const vueComponents = require.context("./components/", true, /\.(vue)$/);
+  vueComponents.keys().forEach((key) => {
+    const component = vueComponents(key).default;
+
+    /* eslint-disable */
+    // if a component has a name defined use the name, else use the path as the component name
+    const name = component.name
+      ? component.name
+      : key
+        .replace(/\.(\/|vue|js)/g, '')
+        .replace(/(\/|-|_|\s)\w/g, match => match.slice(1).toUpperCase());
+
+    app.component(name, component);
+    console.log(name)
+  });
+  /* eslint-enable */
+
+  return app;
+};
+
+/**
+ * create and mount vue instance(s)
+ */
+const vueElements = document.querySelectorAll("[vue]");
+if (vueElements) vueElements.forEach((el) => createVueApp().mount(el));
